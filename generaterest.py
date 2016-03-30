253d4fff39efca96e28bc922c473013beddb04a7
Avinash Mandava <avinash.mandava@gmail.com>
2016-03-30 12:55:32 -0700

    working generate rest

--- generaterest.py
+++ generaterest.py
@@ -4,6 +4,7 @@ import random
 import datetime
 from time import sleep
 import sys
+import json
 
 def generate_sensor_data(num_sensors):
     time = datetime.datetime.now()
@@ -18,27 +19,21 @@ def generate_sensor_data(num_sensors):
     return readings
 
 def create_request(reading):
-    #reading_string = '{ \"value_schema\": "{\"type\": \"record\", \"name\": \"Reading\", \"fields\": [{\"name\": \"device_id\", \"type\": \"string\"},{\"name\": \"time\", \"type\": \"string\"},{\"name\": \"metric_name\", \"type\": \"string\"},{\"name\": \"metric_value\", \"type\": \"string\"}]}", "records": [ {"value": {"device_id":' + reading[0] + ' ,"device_id":' + reading[1] + ',"device_id":' + reading[2] + ',"device_id":' + reading[3] + ' }}]}'
-    reading_string = '{"records": [ {"value":"' + reading[0] +','+ reading[1] +','+ reading[2] +','+ reading[3] + '" }]}'
-    #reading_string = '{"records": [ {"value": {"device_id":' + reading[0] + ' ,"time":' + reading[1] + ',"metric_name":' + reading[2] + ',"metric_value":' + reading[3] + ' }}]}'
+    #reading_string = '{"records": [ {"value":"' + reading[0] +','+ reading[1] +','+ reading[2] +','+ reading[3] + '" }]}'
+    reading_string = {"value_schema": "{\"type\": \"record\", \"name\": \"Reading\", \"fields\": [{\"name\": \"device_id\", \"type\": \"string\"},{\"name\": \"metric_time\", \"type\": \"string\"},{\"name\": \"metric_name\", \"type\": \"string\"},{\"name\": \"metric_value\", \"type\": \"string\"}]}", "records": [{"value": {"device_id": reading[0],"metric_time": reading[1],"metric_name": reading[2],"metric_value": reading[3]}}]}
+    reading_string = json.dumps(reading_string)
     return reading_string
 
-
-    #reading_string = '{ "value_schema": "{"type": "record", "name": "Reading", "fields": [{"name": "device_id", "type": "string"},{"name": "time", "type": "string"},{"name": "metric_name", "type": "string"},{"name": "metric_value", "type": "string"}]}", "records": [ {"value": {"device_id":' + reading[0] + ' ,"device_id":' + reading[1] + ',"device_id":' + reading[2] + ',"device_id":' + reading[3] + ' }}]}'
-
-
-
-    #reading_string = '{ "value_schema": "{"type": "record", "name": "Reading", "fields": [{"name": "device_id", "type": "string"},{"name": "time", "type": "string"},{"name": "metric_name", "type": "string"},{"name": "metric_value", "type": "string"}]}", "records": [ {"value": {"device_id": "reading[0]"}}]}'
-
-
 def simulate_rest_writes(readings):
-    url = 'http://localhost:8082/topics/meter_readings'
-    headers = {'Content-Type' : 'application/vnd.kafka.v1+json'}
+    url = 'http://localhost:8082/topics/testmeter'
+    headers = {'Content-Type' : 'application/vnd.kafka.avro.v1+json'}
     for reading in readings:
         reading_payload = create_request(reading)
         response = requests.post(url,data=reading_payload,headers=headers)
+
         print reading_payload
         print response
+        sleep(1)
 
 def main():
     readings = generate_sensor_data(int(sys.argv[1]))
